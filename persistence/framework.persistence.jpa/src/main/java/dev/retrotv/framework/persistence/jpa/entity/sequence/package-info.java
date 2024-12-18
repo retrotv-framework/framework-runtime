c52f2b83ca8c50c8dafe11f2cf54f38d00981d20
yjj8353 <yjj8353@gmail.com>
2024-12-18 15:46:41 +0900

    persistence 수정

---
+++ persistence/framework.persistence.jpa/src/main/java/dev/retrotv/framework/persistence/jpa/entity/sequence/package-info.java
@@ -0,0 +1,6 @@
+@QueryEntities({IdAndDateEntity.class, IdEntity.class})
+package dev.retrotv.framework.persistence.jpa.entity.sequence;
+
+import com.querydsl.core.annotations.QueryEntities;
+import dev.retrotv.framework.persistence.jpa.entity.table.IdAndDateEntity;
+import dev.retrotv.framework.persistence.jpa.entity.table.IdEntity;
