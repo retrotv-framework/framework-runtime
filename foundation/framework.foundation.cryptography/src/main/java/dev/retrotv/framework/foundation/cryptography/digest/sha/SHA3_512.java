895a48e0cb090bbaf33e1f0b90b3d08c6bbf8ceb
yjj8353 <yjj8353@gmail.com>
2025-04-05 12:21:12 +0900

    리팩터링

--- foundation/framework.foundation.cryptography/src/main/java/dev/retrotv/framework/foundation/cryptography/digest/sha/SHA3_512.java
+++ foundation/framework.foundation.cryptography/src/main/java/dev/retrotv/framework/foundation/cryptography/digest/sha/SHA3_512.java
@@ -1,10 +1,5 @@
 package dev.retrotv.framework.foundation.cryptography.digest.sha;
 
-import java.io.IOException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-
-import dev.retrotv.framework.foundation.common.exception.BaseRuntimeException;
 import dev.retrotv.framework.foundation.cryptography.digest.GeneralDigest;
 import dev.retrotv.framework.foundation.cryptography.digest.enums.DigestAlgorithm;
 
@@ -12,20 +7,5 @@ public class SHA3_512 extends GeneralDigest {
     public SHA3_512() {
         super();
         algorithmName = DigestAlgorithm.SHA3_512.getName();
-    }
-
-    public String getAlgorithmName() {
-        return algorithmName;
-    }
-
-    @Override
-    public byte[] digest(byte[] data) throws IOException {
-        try {
-            MessageDigest md = MessageDigest.getInstance(algorithmName);
-            md.update(data);
-            return md.digest();
-        } catch (NoSuchAlgorithmException ex) {
-            throw new BaseRuntimeException("이 예외는 발생하면 안됩니다. 이 예외가 발생한 경우, 즉시 로직을 확인하시기 바랍니다.", ex);
-        }
     }
 }
