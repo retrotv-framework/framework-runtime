f2503792dfaafe1f88c9ecc2ad030a2c542f78f7
yjj8353 <yjj8353@gmail.com>
2025-08-14 10:45:40 +0900

    설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import com.vanniktech.maven.publish.SonatypeHost
+import java.net.URI
+
 plugins {
     java
     jacoco
@@ -60,4 +63,68 @@ subprojects {
         implementation("org.apache.logging.log4j:log4j-core:$log4j")
         implementation("org.apache.logging.log4j:log4j-slf4j2-impl:$log4j")
     }
+
+    mavenPublishing {
+        publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+        signAllPublications()
+
+        coordinates(group.toString(), project.name, version.toString())
+
+        pom {
+            name.set("framework-runtime")
+            description.set("Spring boot 기반의 프레임워크 런타임 라이브러리입니다.")
+            inceptionYear.set("2025")
+            url.set("https://github.com/retrotv-framework/framework-runtime")
+
+            licenses {
+                license {
+                    name.set("The Apache License, Version 2.0")
+                    url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                }
+            }
+
+            developers {
+                developer {
+                    id.set("yjj8353")
+                    name.set("JaeJun Yang")
+                    email.set("yjj8353@gmail.com")
+                }
+            }
+
+            scm {
+                connection.set("scm:git:git://github.com/retrotv-framework/framework-runtime.git")
+                developerConnection.set("scm:git:ssh://github.com/retrotv-framework/framework-runtime.git")
+                url.set("https://github.com/retrotv-framework/framework-runtime.git")
+            }
+        }
+
+        publishing {
+            repositories {
+
+                // Github Packages에 배포하기 위한 설정
+                maven {
+                    name = "GitHubPackages"
+                    url = URI("https://maven.pkg.github.com/retrotv-framework/framework-runtime")
+                    credentials {
+                        username = System.getenv("USERNAME")
+                        password = System.getenv("PASSWORD")
+                    }
+                }
+            }
+        }
+    }
+
+    tasks.withType<Sign>().configureEach {
+        onlyIf {
+            // 로컬 및 깃허브 패키지 배포 시에는 서명하지 않도록 설정
+            val graph = gradle.taskGraph
+            !graph.allTasks.any { task ->
+                task.path.endsWith("PublicationToMavenLocal") || task.path.endsWith("PublicationToGitHubPackagesRepository")
+            }
+        }
+    }
 }
+
+apply(from = "${rootDir}/gradle/sonarcloud.gradle")
+apply(from = "${rootDir}/gradle/jacoco.gradle")
