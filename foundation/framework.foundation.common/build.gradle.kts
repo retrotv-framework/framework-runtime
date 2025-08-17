4035f69b853e02990bd182bdc6e20e9e1e9f5ee1
yjj8353 <yjj8353@gmail.com>
2025-08-17 15:57:44 +0900

    버전 업

--- foundation/framework.foundation.common/build.gradle.kts
+++ foundation/framework.foundation.common/build.gradle.kts
@@ -6,6 +6,6 @@ val springBoot = project.property("spring-boot.version") as String
 val jackson = project.property("jackson.version") as String
 
 dependencies {
-    compileOnly("com.fasterxml.jackson.core:jackson-databind:$jackson")
     compileOnly("org.springframework.boot:spring-boot-starter-logging:$springBoot")
+    implementation("com.fasterxml.jackson.core:jackson-databind:$jackson")
 }
--- foundation/framework.foundation.cryptography/build.gradle.kts
+++ foundation/framework.foundation.cryptography/build.gradle.kts
@@ -6,6 +6,7 @@ val dataUtils = project.property("data-utils.version") as String
 val cryptography = project.property("cryptography.version") as String
 val bouncycastle = project.property("bouncycastle.version") as String
 
+val jackson = project.property("jackson.version") as String
 val mokito = project.property("mockito.version") as String
 val junit = project.property("junit.version") as String
 val junitLauncher = project.property("junit-launcher.version") as String
@@ -14,6 +15,7 @@ val assertj = project.property("assertj.version") as String
 dependencies {
     implementation(project(":foundation:framework.foundation.common"))
 
+    implementation("com.fasterxml.jackson.core:jackson-module-kotlin:$jackson")
     implementation("dev.retrotv:data-utils:$dataUtils")
     implementation("dev.retrotv:cryptography-core:$cryptography")
     implementation("dev.retrotv:cryptography-hash:$cryptography")
