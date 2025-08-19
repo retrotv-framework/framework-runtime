b25136cb6100c7589f1ebf43bfac6bfd5b6d51bc
yjj8353 <yjj8353@gmail.com>
2025-08-19 13:32:48 +0900

    미사용중인 라이브러리 임시 비활성화

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "dev.retrotv"
-version = "0.0.25-alpha"
+version = "0.0.34-alpha"
 
 // Github Action 버전 출력용
 tasks.register("printVersionName") {
--- foundation/framework.foundation.common/build.gradle.kts
+++ foundation/framework.foundation.common/build.gradle.kts
@@ -8,4 +8,6 @@ val jackson = project.property("jackson.version") as String
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-logging:$springBoot")
     implementation("com.fasterxml.jackson.core:jackson-databind:$jackson")
+    implementation("dev.retrotv:data-utils:0.24.2-alpha")
+    implementation("dev.retrotv:random-value:1.2.1")
 }
