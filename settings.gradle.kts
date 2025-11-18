0244a6c28634133846bdb7ede14c0a2f638c7926
yjj8353 <yjj8353@gmail.com>
2025-11-18 14:46:22 +0900

    kts 제거

--- settings.gradle.kts
+++
@@ -1,6 +0,0 @@
-rootProject.name = "framework-runtime"
-include("foundation:framework.foundation.common")
-include("foundation:framework.foundation.file")
-include("foundation:framework.foundation.cryptography")
-include("foundation:framework.foundation.session")
-include("persistence:framework.persistence.jpa")
